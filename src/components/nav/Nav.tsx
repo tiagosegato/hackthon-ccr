5f75408c1033480bb639ffdde83f9f8528c0a048
Leandro Alves <leandro.alves@ipam.org.br>
2020-06-13 19:19:37 -0300

    novo componente

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
@@ -7,7 +7,13 @@ import LocationOnIcon from "@material-ui/icons/LocationOn";
 import SpeedIcon from "@material-ui/icons/Speed";
 import { Container, Row, Col } from "reactstrap";
 import ImageAvatars from "./avatar";
+import Modal from "react-bootstrap/Modal";
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalHeader from "react-bootstrap/ModalHeader";
+import ModalFooter from "react-bootstrap/ModalFooter";
+import ModalTitle from "react-bootstrap/ModalTitle";
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const useStyles = makeStyles({
   root: {
@@ -18,16 +24,39 @@ const useStyles = makeStyles({
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const showModalCancel = () => {
+    setIsOpen(true);
+  };
+
+  const hideModalCancel = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <Container className="nav-bar">
+    <div className="container-fluid nav-bar">
       <Col xs="12" md="12" lg="12">
         <ImageAvatars />
       </Col>
-       <Col style={{marginTop: '10px'}} className="back" xs="12" md="12" lg="12">
-        <span className="text-name"><b>HS785K</b></span>
+      <Col
+        style={{ marginTop: "10px" }}
+        className="back"
+        xs="12"
+        md="12"
+        lg="12"
+      >
+        <span className="text-name">
+          <b>HS785K</b>
+        </span>
       </Col>
-      <Col style={{marginBottom: '12px'}} className="back" xs="12" md="12" lg="12">
+      <Col
+        style={{ marginBottom: "12px" }}
+        className="back"
+        xs="12"
+        md="12"
+        lg="12"
+      >
         <span className="text-name">BINO DA SILVA</span>
       </Col>
       <Col xs="12" md="12" lg="12">
@@ -50,12 +79,32 @@ export default function SimpleBottomNavigation() {
             icon={<SpeedIcon />}
           />
           <BottomNavigationAction
+            type="button"
+            onClick={showModalCancel}
             className="btn-blue"
             label="Cancelar Consulta"
             icon={<CalendarTodayIcon />}
           />
         </BottomNavigation>
       </Col>
-    </Container>
+      {/* <button onClick={showModalCancel}>Display Modal</button> */}
+      <Modal show={isOpen} onHide={hideModalCancel}>
+        <Modal.Header>
+          <Modal.Title>Tem certeza que deseja cancelar?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Algumas doenças são silenciosas e não possuem sintomas até já estarem
+          completamente instaladas e afetando sua saúde. Por isso é muito
+          importante consultar um médico e fazer alguns exames com
+          periodicidade, mesmo não apresentando nada fora do normal.
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-secondary" onClick={hideModalCancel}>
+            Fechar
+          </button>
+          <button className="btn btn-danger">Cancelar</button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
