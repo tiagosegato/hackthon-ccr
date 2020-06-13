4f01cba84e00b7fa696922e6f278d4a190d3a531
valderli <derlly@ipam.org.br>
2020-06-13 10:34:18 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
+const PanelMap = lazy(() => import('./screens/PanelMap'));
+const Home = lazy(() => import('./screens/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/panelmap">Map</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/panelmap">
+            <PanelMap />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/screens/PanelMap.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import useSwr from "swr";
+import { Icon } from "leaflet";
+
+export const icon = new Icon({
+    iconUrl: "/skateboarding.svg",
+    iconSize: [25, 25]
+  });
+
+const fetcher = (args: any) => fetch(args).then(response => response.json());
+
+const PanelMap = () => {
+
+    const [activePoint, setActivePoint]: any = useState(null);
+
+    const url =
+        "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+    const { data, error } = useSwr(url, { fetcher });
+    const crimes = data && !error ? data.slice(0, 100) : [];
+
+    return (<Map center={[52.63, -1.125]} zoom={14}>
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {crimes.map((p: any) => (
+            <Marker
+                key={p.id}
+                position={[p.location.latitude, p.location.longitude]}
+                onClick={() => {
+                    setActivePoint(p);
+                }}
+                //icon={icon}
+            />
+        ))}
+
+        {activePoint && (
+            <Popup
+                position={[
+                    activePoint.location.latitude,
+                    activePoint.location.longitude
+                ]}
+                onClose={() => {
+                    setActivePoint(null);
+                }}
+            >
+                <div>
+                    <h2>{activePoint.category}</h2>
+                    <p>{activePoint.month}</p>
+                </div>
+            </Popup>
+        )}
+    </Map>)
+};
+
+export default PanelMap;
